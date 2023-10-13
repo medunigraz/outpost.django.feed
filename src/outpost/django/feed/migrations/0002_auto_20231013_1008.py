abeda3cae693876a3468fa566e093888b80c3ecd
Michael Fladischer <michael@fladi.at>
2023-10-13 10:41:46 +0200

    Reformat with black.

--- src/outpost/django/feed/migrations/0002_auto_20231013_1008.py
+++ src/outpost/django/feed/migrations/0002_auto_20231013_1008.py
@@ -6,18 +6,18 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('feed', '0001_initial'),
+        ("feed", "0001_initial"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='article',
-            name='body',
+            model_name="article",
+            name="body",
             field=models.TextField(null=True),
         ),
         migrations.AlterField(
-            model_name='article',
-            name='published',
+            model_name="article",
+            name="published",
             field=models.DateTimeField(null=True),
         ),
     ]
