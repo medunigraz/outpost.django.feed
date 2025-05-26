d11dee986222cbecf573f48aecec70a530a1f3d9
Michael Fladischer <michael@fladi.at>
2025-05-26 21:38:01 +0200

    Deny articles with no publication date.

--- src/outpost/django/feed/views.py
+++ src/outpost/django/feed/views.py
@@ -78,6 +78,8 @@ class ReceiverView(APIView):
     def synchronize(self, model, data):
         defaults = dict()
         entry = data.get("entry", {})
+        if not entry.get("publishedAt"):
+            return HttpResponse("Entry not published yet", status=400)
         for field in model._meta.fields:
             if hasattr(model, "Mapping") and (
                 converter := getattr(model.Mapping, field.attname, None)
